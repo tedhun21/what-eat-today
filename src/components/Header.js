1cf3e4027b29374c07d935df47a458f27bce406a
yebin <yebind3308@gmail.com>
2023-07-19 16:43:09 +0900

    💄 header 레이아웃 제작

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,28 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import "../App.css"
+
 const Header = () => {
     return (
         <>
+            <header>
+                <Container fluid>
+                    <Row>
+                        <Col>
+                            <span id="text-left">날씨 : 비</span></Col>
+                        <Col sm={1} >
+                            <img id='LogoImg' src="/images/HeaderLogo.png" alt="로고" />
+                        </Col>
+                        <Col sm="auto" >
+                            <h1>오늘 뭐 먹지</h1>
+                        </Col>
+                        <Col>
+                            <span id="text-right">현재시간 : 오후 2시 30분</span>
+                        </Col>
+                    </Row>
+                </Container>
+            </header >
         </>
     )
 }
