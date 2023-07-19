78a1bde7c431f8952e1f28faaa0be087bcb1fe5c
yebin <yebind3308@gmail.com>
2023-07-19 18:01:57 +0900

    💄 Start 레이아웃 제작중

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,8 +1,27 @@
 const Start = () => {
     return (
-        <>
-
-        </>
+        <main>
+            <div className="Background">
+                <div className="Outline">
+                    <div className="Inline">
+                        <div>
+                            <div>
+                                <h1>오늘 뭐 먹지?</h1>
+                                <h2>스터디_참숯가마</h2>
+                            </div>
+                            <div>
+                                <span>
+                                    메뉴 추천 받으러 가기
+                                </span>
+                            </div>
+                            <div>
+                                <bold>START</bold>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
     )
 }
 
