625e4cb50d23dbcdba1a2701bef1d7c7a578b99b
yebin <yebind3308@gmail.com>
2023-07-20 01:05:03 +0900

    💄 Start.js 레이아웃 작성

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,26 +1,26 @@
 const Start = () => {
-  return (
-    <main>
-      <div className="Background">
-        <div className="Outline">
-          <div className="Inline">
-            <div>
-              <div>
-                <h1>오늘 뭐 먹지?</h1>
-                <h2>스터디_참숯가마</h2>
-              </div>
-              <div>
-                <span>메뉴 추천 받으러 가기</span>
-              </div>
-              <div>
-                <strong>START</strong>
-              </div>
+    return (
+        <main>
+            <div className="Background">
+                <div className="Outline">
+                    <div className="Inline">
+                        <div className="StartWrap">
+                            <div className="TitleWrap">
+                                <h1>오늘 뭐 먹지?</h1>
+                                <h2>스터디_참숯가마</h2>
+                            </div>
+                            <div className="TitleText">
+                                <span>메뉴 추천 받으러 가기</span>
+                            </div>
+                            <div className="StartBtn">
+                                <span>START</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+        </main>
+    );
 };
 
 export default Start;
