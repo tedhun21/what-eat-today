320db6e7bd512807ce9edccc260c35362fe36d6d
yebin <yebind3308@gmail.com>
2023-07-23 22:28:16 +0900

    🚑 Background z-index 수정

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,30 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Button from "react-bootstrap/Button";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 const Start = () => {
-  return (
-    <main>
-      <div className="Container">
-        <div className="Outline">
-          <div className="Inline">
-            <div className="StartWrap">
-              <div className="TitleWrap">
-                <h1>오늘 뭐 먹지?</h1>
-                <h2>스터디_참숯가마</h2>
-              </div>
-              <div className="TitleText">
-                <span>메뉴 추천 받으러 가기</span>
-              </div>
-              <Link to="/Decided">
-                <Button variant="danger">START</Button>
-              </Link>
+    return (
+        <main>
+            <div className="Container" >
+                <div className="Outline">
+                    <div className="Inline">
+                        <div className="StartWrap">
+                            <div className="TitleWrap">
+                                <h1>오늘 뭐 먹지?</h1>
+                                <h2>스터디_참숯가마</h2>
+                            </div>
+                            <div className="TitleText">
+                                <span>메뉴 추천 받으러 가기</span>
+                            </div>
+                            <Link to="/Decided">
+                                <Button variant="danger">START</Button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+        </main>
+    );
 };
 
 export default Start;
