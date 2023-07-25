decdb632ab87f2574bae6f660223c934979687ab
tedhun21 <songjihun940409@gmail.com>
2023-07-25 23:26:54 +0900

    🚑 key 에러 수정

--- src/pages/DecidedResult.js
+++ src/pages/DecidedResult.js
@@ -1,39 +1,36 @@
 import React from "react";
-import { Link, useParams } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
+import { Link, useParams } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 import { foods } from "../data/Data";
 
-
-
-
 const DecidedResult = () => {
+  const { section } = useParams();
+  const filteredFood = foods.filter((food) => food.section === section)[0].subclass;
 
-    const { section } = useParams()
-    const filteredFood = foods.filter((food) => food.section === section)[0].subclass
-
-
-    return (
-        <main>
-            <div className="Container">
-                <div className="Outline">
-                    <div className="Inline">
-                        <div className="DecidedWrap">
-                            <Link to="/decided">
-                                <div className="BackBtn" />
-                            </Link>
-                            <div className="ResultWrap">
-                                {filteredFood.map((food) =>
-                                    <Link to={`/result/${food}`} >
-                                        <Button variant="danger">{food}</Button>
-                                    </Link>
-                                )}
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <main>
+      <div className="Container">
+        <div className="Outline">
+          <div className="Inline">
+            <div className="DecidedWrap">
+              <Link to="/decided">
+                <div className="BackBtn" />
+              </Link>
+              <ul className="ResultWrap">
+                {filteredFood.map((food,index) => (
+                  <li key={index}>
+                    <Link to={`/result/${food}`} >
+                      <Button variant="danger">{food}</Button>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
             </div>
-        </main>
-    )
-}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default DecidedResult;
